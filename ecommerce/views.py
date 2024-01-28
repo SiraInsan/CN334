2f71844106054ff1f36161ee06abf4e2ebe7a8d6
SiraInsan <99487653+SiraInsan@users.noreply.github.com>
2024-01-28 13:54:26 +0000

    Page views

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
 def ecommerce_index_view(request):
     '''This function render index page of ecommerce views'''
 
@@ -12,3 +12,38 @@ def item_view(request, item_id):
  "item_id": item_id
  }
  return render(request, 'index.html',context = context_data)
+
+def home_page_view(request):
+    context_data = {
+        "page_title": "Home Page",
+        
+    }
+    return render(request, 'home.html', context=context_data)
+
+def category_page_view(request):
+    context_data = {
+        "page_title": "Category Page",
+        
+    }
+    return render(request, 'category.html', context=context_data)
+
+def product_page_view(request):
+    context_data = {
+        "page_title": "Product Page",
+        
+    }
+    return render(request, 'product.html', context=context_data)
+
+def checkout_page_view(request):
+    context_data = {
+        "page_title": "Checkout Page",
+        
+    }
+    return render(request, 'checkout.html', context=context_data)
+
+def contact_page_view(request):
+    context_data = {
+        "page_title": "Contact Page",
+        
+    }
+    return render(request, 'contact.html', context=context_data)
